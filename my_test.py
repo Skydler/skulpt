2c6da78562f2f37b26ff5b891d30d2ee8ec72e54
acbart <acbart@vt.edu>
2014-12-10 12:07:23 -0500

    Adding in matplotlib support

---
+++ my_test.py
@@ -0,0 +1,8 @@
+import weather
+
+print "Forecasts:", weather.get_forecasts('Blacksburg, VA')
+print "Temperature:", weather.get_temperature('Blacksburg, VA')
+
+import matplotlib.pyplot as plt
+
+#print plt.plot
