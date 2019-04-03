79b9dee4b59fb6b538399f5d07fa49051a6a356e
Austin Cory Bart <acbart@vt.edu>
2019-04-03 15:46:38 -0400

    Update pedal

--- src/lib/pedal/plugins/vpl_safe_runner.py
+++ src/lib/pedal/plugins/vpl_safe_runner.py
@@ -1,10 +1,10 @@
 from pedal import run
-from pedal import find_file
+from pedal import set_source_file
 import sys
 
 if __name__ == "__main__":
-    find_file(sys.argv[1] if len(sys.argv) > 1 else 'main.py')
-    student = run(tracer_style='coverage', context=False)
+    set_source_file(sys.argv[1] if len(sys.argv) > 1 else 'main.py')
+    student = run(context=False)
     print(student.raw_output)
     if student.exception:
         print(student.exception_formatted, file=sys.stderr)
