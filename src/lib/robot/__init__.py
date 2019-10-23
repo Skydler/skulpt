6eeabde2c5eb80c4a13f0958b75a69d99cd31b8a
ramiro gonzalez perez <ramegp@gmail.com>
2019-10-23 09:16:36 -0300

    Agrego el + a la funcion

--- src/lib/robot/__init__.py
+++ src/lib/robot/__init__.py
@@ -2,23 +2,23 @@ import urllib.request as url
 
 
 def _make_request(direction, seconds):
-    elem = url.urlopen("https://jsonplaceholder.typicode.com/posts/42")
+    elem = url.urlopen('http://192.168.1.1/'+direction+'/+'+str(seconds))
     print(elem.read())
     print(direction)
     print(seconds)
 
 
 def avanzar(seconds):
-    _make_request('adelante', seconds)
+    _make_request('forward', seconds)
 
 
 def retroceder(seconds):
-    _make_request('atras', seconds)
+    _make_request('backward', seconds)
 
 
 def derecha(seconds):
-    _make_request('derecha', seconds)
+    _make_request('right', seconds)
 
 
 def izquierda(seconds):
-    _make_request('izquierda', seconds)
+    _make_request('left', seconds)
